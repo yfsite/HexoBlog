101f0ae9d60348c84f8dc9fc44f0d53c4b82869b
root <root@coding.ide>
2020-04-24 07:09:20 +0800

    Site updated: 2020-04-24 07:09:20

--- js/animate.js
+++ js/animate.js
@@ -1,3 +1,3 @@
-// build time:Thu Apr 23 2020 21:06:03 GMT+0800 (CST)
+// build time:Fri Apr 24 2020 07:09:20 GMT+0800 (CST)
 $(function(){if(mihoConfig.isHome===true&&document.body.clientWidth>700){var e=["pulse","fadeInRight","flipInX","lightSpeedIn","rotateInUpLeft","slideInUp","zoomIn"],a=e.length,n=e[Math.ceil(Math.random()*a)-1];window.sr=ScrollReveal();sr.reveal("#header-row",{duration:0,afterReveal:function(e){$(e).addClass("animated "+n)}});sr.reveal(".body-wrap > article",{duration:0,afterReveal:function(e){$(e).addClass("animated "+n)}})}});
 //rebuild by neat 
